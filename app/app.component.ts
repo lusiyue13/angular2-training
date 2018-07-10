cfea36d75a6d4031e5244c88975559da76fac156
lu.siyue@wustl.edu <lu.siyue@wustl.edu>
2018-07-10 15:58:00 -0700

    angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
+}
